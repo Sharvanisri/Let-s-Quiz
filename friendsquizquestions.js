4d4f95e9adb69591f9b80679bde15fb8f2aca59f
Sharvanisri <sharvanisri28@gmail.com>
2021-05-01 19:37:43 +0530

    Added all the web pages, images and javasciprt files.
    Home page and the math quiz are ready.

---
+++ friendsquizquestions.js
@@ -0,0 +1,29 @@
+
+
+const quiz=[
+{
+	q:'What is Chandler's middle name?',
+	options:['Muriel','Jason','Kim','Zachary'],
+	answer:0
+},
+{
+	q:'What's the title of the Friends theme song?',
+	options:['I'll be there for them','I'll be there for those','I'll be there for you','I'll be there for that'],
+	answer:2
+},
+{
+	q:'What is the name of Rachel's hairless cat?',
+	options:['Baldy','Mrs. Whishkerson','Ms. Whishkerson','Felix'],
+	answer:1
+},
+{
+	q:'Who is Chandler and Joey's TV magazine addressed to?',
+	options:['Miss Chanandler Bong','Mrs. Chanel Bing','Ms. Chandelier Bang','Rachel Greep'],
+	answer:0
+},
+{
+	q:'What is the name of Phoebe's alter-ego?',
+	options:['Princess Consuela Bananahammock','Regina Falange','Monica Bing','Elaine Benes'],
+	answer:1
+}
+]
---
+++ mathquizquestions.js
@@ -0,0 +1,29 @@
+
+
+const quiz=[
+{
+	q:'Solve : 200 – (96 ÷ 4)',
+	options:['105','176','26','16'],
+	answer:1
+},
+{
+	q:'Simplify : 3 + 6 x (5 + 4) ÷ 3 - 7',
+	options:['11','16','14','15'],
+	answer:2
+},
+{
+	q:'Simplify: 26 + 32 - 12',
+	options:['0','32','56','46'],
+	answer:3
+},
+{
+	q:'Solve : 24 + 4 ÷ 4',
+	options:['25','6','28','7'],
+	answer:0
+},
+{
+	q:'Simplify :150 ÷ (6 + 3 x 8) - 5',
+	options:['2','5','0','NONE OF THESE'],
+	answer:2
+}
+]
